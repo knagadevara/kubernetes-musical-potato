c02048e2c1c98ffe0ff2b8a8aef73079aa41bf55
knagadevara <vnsk.1991@gmail.com>
2020-08-15 12:53:52 +0530

    Disabling SELinux

--- installation_utilities/sestat.sh
+++ installation_utilities/sestat.sh
@@ -1,2 +1,13 @@
 kubeNodes=$1
-for kubHost in $(cat ${kubeNodes}) ; do ssh ${kubHost} 'echo -e "\n\t" ; uname -n ; echo -e "\t" ; SEstatus=$(getenforce) ; if [[ ${SEstatus} != 'Disabled' ]] ; then setenforce 0 ; echo -e "Its Disabled now on ${HOST}\n" ; else echo "SELinux is disabled already on this HOST"; fi' ; done
+for kubHost in $(cat ${kubeNodes}) ;
+do ssh ${kubHost} 'echo -e "\n\t" ; \
+	uname -n ; echo -e "\t" ; \
+	SEstatus=$(getenforce) ; \
+	if [[ ${SEstatus} != 'Disabled' ]] ; \
+	then \
+		setenforce 0 ; \
+		echo -e "Its Disabled now on ${HOST}\n" ; \
+	else \
+		echo "SELinux is disabled already on this HOST"; \
+	fi' ;\
+done
